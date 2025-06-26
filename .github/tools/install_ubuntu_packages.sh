532778214a6b195057b0a77dd28e3874616b452a
Mariusz Tkaczyk <mariusz.tkaczyk@dell.com>
2025-06-26 06:29:37 +0100

    mdadm: use lseek consistently
    mdadm used both lseek and lseek64 for legacy reasons. These days, we just
    need to configure __USE_LARGEFILE64 macro. Fixing this issue enables
    musl compilation.
    
    Add macro, and change all lseek64 to lseek. Fix style issues in these
    lines.
    
    Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@dell.com>

--- .github/tools/install_ubuntu_packages.sh
+++ .github/tools/install_ubuntu_packages.sh
@@ -6,7 +6,13 @@ echo "Detected VERSION_CODENAME: $VERSION_CODENAME"
 # Add ubuntu repository
 sudo add-apt-repository -y "deb [arch=amd64] http://archive.ubuntu.com/ubuntu $VERSION_CODENAME \
         main universe"
-# Install gcc
-sudo apt-get -y install gcc-$1 --no-upgrade --no-install-recommends --no-install-suggests
+
 # Install dependencies
 sudo apt-get -y install make gcc libudev-dev devscripts --no-upgrade --no-install-recommends --no-install-suggests
+
+# Install test specific
+if [[ "$1" == "musl" ]]; then
+        sudo apt-get -y install make gcc musl musl-dev musl-tools build-essential linux-headers-generic
+else
+        sudo apt-get -y install gcc-$1 --no-upgrade --no-install-recommends --no-install-suggests
+fi
