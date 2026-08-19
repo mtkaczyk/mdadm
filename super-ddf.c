406ab49fa94b8ba5eda88d82673d9d2b1692e185
cenhuilin <cenhuilin@kylinos.cn>
2026-08-19 16:30:58 +0800

    super-ddf: clamp max_pd_entries in load_ddf_local to prevent OOB read
    max_pd_entries is taken from an untrusted DDF header without clamping it
    to the actual size of the phys_section. This could lead to a heap
    out-of-bounds read when iterating over phys_disk entries.
    
    Use ddf_safe_entry_count() to clamp the loop boundary.
    
    Signed-off-by: cenhuilin <cenhuilin@kylinos.cn>

--- super-ddf.c
+++ super-ddf.c
@@ -1195,6 +1195,7 @@ static int load_ddf_local(int fd, struct ddf_super *super,
 	int vnum;
 	unsigned int max_virt_disks =
 		be16_to_cpu(super->active->max_vd_entries);
+	unsigned int max_pdes;
 	unsigned long long dsize;
 
 	/* First the local disk info */
@@ -1237,7 +1238,12 @@ static int load_ddf_local(int fd, struct ddf_super *super,
 		dl->vlist[i] = NULL;
 	super->dlist = dl;
 	dl->pdnum = -1;
-	for (i = 0; i < be16_to_cpu(super->active->max_pd_entries); i++)
+	max_pdes = ddf_safe_entry_count(super->pdsize,
+			offsetof(struct phys_disk, entries),
+			sizeof(super->phys->entries[0]),
+			be16_to_cpu(super->active->max_pd_entries));
+
+	for (i = 0; i < max_pdes; i++)
 		if (memcmp(super->phys->entries[i].guid,
 			   dl->disk.guid, DDF_GUID_LEN) == 0)
 			dl->pdnum = i;
