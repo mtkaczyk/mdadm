9d60fb9db8481cf2a488afc337bf0c8e2bb160fa
Mariusz Tkaczyk <mtkaczyk@kernel.org>
2026-08-31 10:28:59 +0200

    util.c: Rework check_ext2
    Just check magic and report that is has been found.
    
    Signed-off-by: Mariusz Tkaczyk <mtkaczyk@kernel.org>

--- mdadm.h
+++ mdadm.h
@@ -1528,7 +1528,7 @@ extern int parse_layout_10(char *layout);
 extern int parse_layout_faulty(char *layout);
 extern int parse_num(int *dest, const char *num);
 extern int parse_cluster_confirm_arg(char *inp, char **devname, int *slot);
-extern int check_ext2(int fd, char *name);
+extern bool check_ext2(int fd, char *name);
 extern int check_reiser(int fd, char *name);
 extern int check_raid(int fd, char *name);
 extern int check_partitions(int fd, char *dname,
